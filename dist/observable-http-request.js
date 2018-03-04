b5797791dad975ffb552e42c812ba840b14459fd
penrico <enrico.piccinin@gmail.com>
2018-03-04 11:09:46 +0100

    test coverage added

--- dist/observable-http-request.js
+++ dist/observable-http-request.js
@@ -13,7 +13,7 @@ exports.httpGetRequestObs = httpGetRequestObs;
 const requestGetFunction = (uri, options, callback) => request.get(uri, options, callback);
 const requestGetObs = Observable_1.Observable.bindNodeCallback(requestGetFunction, toBodyOnly);
 // ============================ http POST ================================
-// Returns an Observable which emits when the data is ready
+// Returns an Observable which emits when the operation is completed and the response returned
 function httpPostRequestObs(uri, body) {
     return requestPostObs(uri, { json: true, body });
 }
@@ -21,7 +21,37 @@ exports.httpPostRequestObs = httpPostRequestObs;
 // https://stackoverflow.com/questions/43462628/cannot-create-observable-from-observable-bindnodecallbackfs-readfile-in-typesc
 const requestPostFunction = (uri, options, callback) => request.post(uri, options, callback);
 const requestPostObs = Observable_1.Observable.bindNodeCallback(requestPostFunction, toBodyOnly);
-function toBodyOnly(_res, _body) {
-    return _body;
+// ============================ http PUT ================================
+// Returns an Observable which emits when the operation is completed and the response returned
+function httpPutRequestObs(uri, body) {
+    return requestPutObs(uri, { json: true, body });
+}
+exports.httpPutRequestObs = httpPutRequestObs;
+// https://stackoverflow.com/questions/43462628/cannot-create-observable-from-observable-bindnodecallbackfs-readfile-in-typesc
+const requestPutFunction = (uri, options, callback) => request.put(uri, options, callback);
+const requestPutObs = Observable_1.Observable.bindNodeCallback(requestPutFunction, toBodyOnly);
+// ============================ http PATCH ================================
+// Returns an Observable which emits when the operation is completed and the response returned
+function httpPatchRequestObs(uri, body) {
+    return requestPatchObs(uri, { json: true, body });
+}
+exports.httpPatchRequestObs = httpPatchRequestObs;
+// https://stackoverflow.com/questions/43462628/cannot-create-observable-from-observable-bindnodecallbackfs-readfile-in-typesc
+const requestPatchFunction = (uri, options, callback) => request.patch(uri, options, callback);
+const requestPatchObs = Observable_1.Observable.bindNodeCallback(requestPatchFunction, toBodyOnly);
+// ============================ http DELETE ================================
+// Returns an Observable which emits when the operation is completed and the response returned
+function httpDeleteRequestObs(uri, body) {
+    return requestDeleteObs(uri, { json: true, body });
+}
+exports.httpDeleteRequestObs = httpDeleteRequestObs;
+// https://stackoverflow.com/questions/43462628/cannot-create-observable-from-observable-bindnodecallbackfs-readfile-in-typesc
+const requestDeleteFunction = (uri, options, callback) => request.delete(uri, options, callback);
+const requestDeleteObs = Observable_1.Observable.bindNodeCallback(requestDeleteFunction, toResponseAndBody);
+function toBodyOnly(_res, body) {
+    return body;
+}
+function toResponseAndBody(response, body) {
+    return { response, body };
 }
 //# sourceMappingURL=observable-http-request.js.map
--- dist/observable-http-request.spec.js
+++ dist/observable-http-request.spec.js
@@ -2,6 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 require("mocha");
 const observable_http_request_1 = require("./observable-http-request");
+const observable_http_request_2 = require("./observable-http-request");
+const observable_http_request_3 = require("./observable-http-request");
+const observable_http_request_4 = require("./observable-http-request");
+const observable_http_request_5 = require("./observable-http-request");
 describe('httpGetRequestObs function', () => {
     it('issues an http get request to wikipedia - returns an observable', done => {
         const uri = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=pentole&namespace=0&limit=10';
@@ -24,13 +28,67 @@ describe('httpPostRequestObs function', () => {
         const job = 'dev';
         const user = { name, job };
         let serviceCallResult;
-        observable_http_request_1.httpPostRequestObs(uri, user).subscribe(data => serviceCallResult = data, error => console.error(error), () => {
+        observable_http_request_2.httpPostRequestObs(uri, user).subscribe(data => serviceCallResult = data, error => console.error(error), () => {
             if (serviceCallResult.name === name && serviceCallResult.job === job) {
                 return done();
             }
             else {
                 console.error(serviceCallResult);
                 return done(new Error('serviceCallResult empty'));
+            }
+        });
+    });
+});
+describe('httpPutRequestObs function', () => {
+    it('issues an http put request to https://reqres.in/ - returns an observable', done => {
+        const uri = 'https://reqres.in/api/users/2';
+        const name = 'John';
+        const job = 'dev';
+        const user = { name, job };
+        let serviceCallResult;
+        observable_http_request_3.httpPutRequestObs(uri, user).subscribe(data => serviceCallResult = data, error => console.error(error), () => {
+            if (serviceCallResult.name === name && serviceCallResult.job === job) {
+                return done();
+            }
+            else {
+                console.error(serviceCallResult);
+                return done(new Error('serviceCallResult empty'));
+            }
+        });
+    });
+});
+describe('httpPatchRequestObs function', () => {
+    it('issues an http patch request to https://reqres.in/ - returns an observable', done => {
+        const uri = 'https://reqres.in/api/users/2';
+        const name = 'John';
+        const job = 'dev';
+        const user = { name, job };
+        let serviceCallResult;
+        observable_http_request_4.httpPatchRequestObs(uri, user).subscribe(data => serviceCallResult = data, error => console.error(error), () => {
+            if (serviceCallResult.name === name && serviceCallResult.job === job) {
+                return done();
+            }
+            else {
+                console.error(serviceCallResult);
+                return done(new Error('serviceCallResult empty'));
+            }
+        });
+    });
+});
+describe('httpDeleteRequestObs function', () => {
+    it('issues an http delete request to https://reqres.in/ - returns an observable', done => {
+        const uri = 'https://reqres.in/api/users/2';
+        const name = 'John';
+        const job = 'dev';
+        const user = { name, job };
+        let serviceCallResult;
+        observable_http_request_5.httpDeleteRequestObs(uri, user).subscribe(data => serviceCallResult = data, error => console.error(error), () => {
+            if (serviceCallResult.response.statusCode === 204) {
+                return done();
+            }
+            else {
+                console.error(serviceCallResult);
+                return done(new Error('status code expected is 204 and not ' + serviceCallResult));
             }
         });
     });
